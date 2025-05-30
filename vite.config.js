4aecedae0395ecf3088840e5599d1994c37370b2
DRAP-3312 <davidake117@gmail.com>
2025-05-30 14:00:09 -0500

    update vite build

--- vite.config.js
+++ vite.config.js
@@ -1,59 +1,59 @@
 //Production
-// import { defineConfig } from "vite";
-// import vue from "@vitejs/plugin-vue";
-
-// export default defineConfig({
-//   plugins: [
-//     vue({
-//       template: {
-//         compilerOptions: {
-//           // Habilita el modo Web Component
-//           isCustomElement: (tag) => tag.includes("-"),
-//         },
-//       },
-//       customElement: true, // Esto es importante para el manejo de estilos
-//     }),
-//   ],
-//   build: {
-//     lib: {
-//       entry: "src/components/ChatWidget.js",
-//       name: "ChatWidget",
-//       fileName: (format) => `chat-widget.${format}.js`,
-//       formats: ["es", "umd"],
-//     },
-//     cssCodeSplit: true,
-//     rollupOptions: {
-//       external: ["vue"],
-//       output: {
-//         globals: {
-//           vue: "Vue",
-//         },
-//       },
-//     },
-//   },
-// });
-
-//Dev
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // Habilita el modo Web Component
+          isCustomElement: (tag) => tag.includes("-"),
+        },
+      },
+      customElement: true, // Esto es importante para el manejo de estilos
+    }),
+  ],
   build: {
     lib: {
-      entry: "src/App.vue",
-      name: "VueChatWidget",
-      fileName: (format) => `vue-chat-widget.${format}.js`,
+      entry: "src/components/ChatWidget.js",
+      name: "ChatWidget",
+      fileName: (format) => `chat-widget.${format}.js`,
+      formats: ["es", "umd"],
     },
+    cssCodeSplit: true,
     rollupOptions: {
+      external: ["vue"],
       output: {
-        // Asegúrate de que los estilos se extraigan correctamente
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === "style.css") return "vue-chat-widget.css";
-          return assetInfo.name;
+        globals: {
+          vue: "Vue",
         },
       },
     },
-    cssCodeSplit: false,
   },
 });
+
+//Dev
+// import { defineConfig } from "vite";
+// import vue from "@vitejs/plugin-vue";
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   build: {
+//     lib: {
+//       entry: "src/App.vue",
+//       name: "VueChatWidget",
+//       fileName: (format) => `vue-chat-widget.${format}.js`,
+//     },
+//     rollupOptions: {
+//       output: {
+//         // Asegúrate de que los estilos se extraigan correctamente
+//         assetFileNames: (assetInfo) => {
+//           if (assetInfo.name === "style.css") return "vue-chat-widget.css";
+//           return assetInfo.name;
+//         },
+//       },
+//     },
+//     cssCodeSplit: false,
+//   },
+// });
